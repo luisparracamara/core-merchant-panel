d05c4a6eeb6f7f9ea0ad8bc3ff314ead5ea32350
luisparracamara <luisparracamara@gmail.com>
2026-01-25 19:16:33 -0700

    first commit

---
+++ src/main/java/com/core/merchant/panel/CoreMerchantPanelApplication.java
@@ -0,0 +1,13 @@
+package com.core.merchant.panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMerchantPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreMerchantPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/merchant/panel/CoreMerchantPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.merchant.panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreMerchantPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
